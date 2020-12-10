bc43a98f3d0eeaaa1bcb9d30b55eb1282f8009d1
Ravindraayata <73533296+Ravindraayata@users.noreply.github.com>
2020-12-10 08:47:31 +0530

    Update Place order as guest.js

--- Tests/Place order as guest.js
+++ Tests/Place order as guest.js
@@ -8,33 +8,53 @@ import LoginCheckout_Page from "../Page-Objects/Pages/LoginCheckout_Page";
 import ShippingAndBilling_Page from "../Page-Objects/Pages/ShippingAndBilling_Page";
 import Payment_Page from "../Page-Objects/Pages/Payment_Page";
 import OrderConfirmation_Page from "../Page-Objects/Pages/OrderConfirmation_Page";
+//import locatordata from "../TestData/locatorData";
+
 
 describe("Place order as Guest user using credit Card", () => {
-  beforeEach(() => {
-    Home_Page.open("https://p3.stage6.dm.projecta.com/uk/en_gb");
-    // Home_Page.open("https://p3.stage6.dm.projecta.com/us/en");
+  it("Navigates to the DM website", () => {
+     browser.url('/')
+		browser.maximizeWindow();
     Country_Popup.submitCountrySelection();
+  });
+
+  it("Selects the product", () => {
     Header.navigateToMenBoots();
     ProductListing_Page.clickOnProductTile();
+
+  });
+
+  it("Adds product to bag and proceeds to checkout", () => {
+    Product_Page.selectsizegudetab();
     Product_Page.selectProductSize();
     Product_Page.clickOnAddToCart();
     Product_Page.clickOnCheckoutButton();
+  });
+
+  it("Navigates to checkout-login page", () => {
     Bag_Page.clickOnCheckoutButton();
+  });
+
+  it("Proceeds to Shipping & Billing page", () => {
     LoginCheckout_Page.clickOnGuestCheckout();
+  });
+
+  it("Enters shipping details for Guest user", () => {
     ShippingAndBilling_Page.enterNewAddressAsGuest();
   });
 
-  afterEach(() => {
-    browser.reloadSession();
-  });
+    it ("Places order with credit card", () => {
+       Payment_Page.placeOrderUsingCreditCard();
+   });
 
-  it("Places order as guest using credit card", () => {
-    Payment_Page.placeOrderUsingCreditCard();
-    OrderConfirmation_Page.verifyThankYouText();
-  });
+   it ("Places order with PayPal", () => {
+       Payment_Page.placeOrderUsingPaypal();
+     });
 
-  it("Places order as guest using Paypal", () => {
-    Payment_Page.placeOrderUsingPaypal();
+  it("Verify the order details", () => {
+    
+   // browser.pause(15000);
     OrderConfirmation_Page.verifyThankYouText();
+    
   });
 });
