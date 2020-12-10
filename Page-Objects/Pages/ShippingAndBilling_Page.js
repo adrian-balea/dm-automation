91b607d6dfee6b60ca173e3ddd30b1586a426d0e
Ravindraayata <73533296+Ravindraayata@users.noreply.github.com>
2020-12-10 08:28:20 +0530

    Update ShippingAndBilling_Page.js

--- Page-Objects/Pages/ShippingAndBilling_Page.js
+++ Page-Objects/Pages/ShippingAndBilling_Page.js
@@ -8,34 +8,35 @@ class ShippingAndBilling_Page {
   get lastNameField() {
     return $('//*[@id="shippingAddressForm"]//*[@id="address.surname"]');
   }
-  get shippingAddressSearchField() {
-    return $('//*[@id="shippingAddressForm"]//*[@id="loqateAdressSearchTxt"]');
+  get shippingAddressSearchField() {return $('//*[@id="shippingAddressForm"]//*[@id="loqateAdressSearchTxt"]');
   }
-  get suggestedAddress() {
-    return $(
-      "#shipping-i18n-address-form > section.loqate-address.js-loqate-address.active > div.form-group.loqate-form > div.loqate-search-options > div.address-list > div:nth-child(1) > div > span.txt"
-    );
-  }
-  get phoneNumberField() {
-    return $('//*[@id="shippingAddressForm"]//*[@id="address.phone"]');
-  }
-  get continiueToPaymentButton() {
-    return $("#addressSubmit");
-  }
+  get suggestedAddress() {return $("#shipping-i18n-address-form > section.loqate-address.js-loqate-address.active > div.form-group.loqate-form > div.loqate-search-options > div.address-list > div:nth-child(1) > div > span.txt"); }
+ //get selelctaddressfromlist(){return$('#shipping-i18n-address-form > section.loqate-address.js-loqate-address.selected.active > div.form-group.loqate-form > div.loqate-search-options > div.address-list > div:nth-child(1) > div.address-desc > span.txt');}
+
+ get Selectvalue() {return $('//*[@id="shipping-i18n-address-form"]//div[@class="address-desc"]//span[1]');}
+  
+ get phoneNumberField() {return $('//*[@id="shippingAddressForm"]//*[@id="address.phone"]');  }
+  get continiueToPaymentButton() {return $("#addressSubmit");}
+  
+  
 
   enterNewAddressAsGuest() {
     this.emailField.waitForExist();
     this.emailField.setValue("ayatacommerce@mailinator.com");
     this.firstNameField.setValue("John");
     this.lastNameField.setValue("Smith");
-    // this.shippingAddressSearchField.setValue("Test");
-    this.shippingAddressSearchField.setValue("7 Deansgate");
-    this.suggestedAddress.waitForExist();
-    this.suggestedAddress.click();
-    // this.phoneNumberField.setValue("12345678911");
-    this.phoneNumberField.setValue("07771540640");
-    this.continiueToPaymentButton.scrollIntoView();
-    this.continiueToPaymentButton.click();
+     this.shippingAddressSearchField.setValue("Test");
+     browser.pause(1000);
+
+  //this.shippingAddressSearchField.setValue("7 Deansgate");
+  this.suggestedAddress.waitForExist();
+  this.suggestedAddress.click();
+  browser.pause(5000);
+   this.phoneNumberField.setValue("12345678911");
+ // this.phoneNumberField.setValue("07771540640");
+  this.continiueToPaymentButton.scrollIntoView();
+  this.continiueToPaymentButton.click();
+
   }
 }
 export default new ShippingAndBilling_Page();
