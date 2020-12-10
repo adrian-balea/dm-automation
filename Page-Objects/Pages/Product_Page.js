cca9fd5700de77cf412b672fed3863f8f3398f78
Adrian <adrian.balea@gmail.com>
2020-12-10 17:28:58 +0000

    tweaked the Product_Page.js

--- Page-Objects/Pages/Product_Page.js
+++ Page-Objects/Pages/Product_Page.js
@@ -1,40 +1,47 @@
 class Product_Page {
   // Selectors
-//<<<<<<< HEAD
-      
-  get selecttab() {return $('//div[@class="size-title"]//a[1]');}
-  get productSize() {return $('//*[@id="sizeSelector"]//li[2]//a');}
-  get addToCartButton() {return $("#addToCartButton");}
-  get checkoutButton() {return $("a=Checkout");}
-
+  get selecttab() {
+    return $('//div[@class="size-title"]//a[1]');
+  }
   
-//=======
-  //get productSize() {
-    //For US
+  get addToCartButton() {
+    return $("#addToCartButton");
+  }
+  get checkoutButton() {
+    return $("a=Checkout");
+  }
+
+  get productSize() {
+    // For US
     // return $('//*[@id="sizeSelector"]//li//a[@data-sku-code ="800090828511"]');
 
-    //For UK
-   // return $('//*[@id="sizeSelector"]//li//a[@data-sku-code ="190665311945"]');
- // }
+    // For UK
+    return $('//*[@id="sizeSelector"]//li//a[@data-sku-code ="190665311945"]');
+  }
 
-  get selecttab() {return $('//div[@class="size-title"]//a[1]');}
-  get productSize() {return $('//*[@id="sizeSelector"]//li[2]//a');}
-  get addToCartButton() {return $("#addToCartButton");}
-  get checkoutButton() {return $("a=Checkout");}
-
+  get selecttab() {
+    return $('//div[@class="size-title"]//a[1]');
+  }
+  get productSize() {
+    return $('//*[@id="sizeSelector"]//li[2]//a');
+  }
+  get addToCartButton() {
+    return $("#addToCartButton");
+  }
+  get checkoutButton() {
+    return $("a=Checkout");
+  }
 
   // Methods
-
-   selectsizegudetab(){
+  selectsizegudetab() {
     this.selecttab.waitForExist();
     this.selecttab.click();
-   }
+  }
+
   selectProductSize() {
-   // this.selectProductSize.
     this.productSize.waitForExist();
     this.productSize.click();
   }
-
 
   clickOnAddToCart() {
     this.addToCartButton.click();
