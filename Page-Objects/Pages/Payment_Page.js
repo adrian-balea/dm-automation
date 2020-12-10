1a66887e7dc44231183cbc3c8f82412b9379c5a3
Ravindraayata <73533296+Ravindraayata@users.noreply.github.com>
2020-12-10 08:39:38 +0530

    Update Payment_Page.js

--- Page-Objects/Pages/Payment_Page.js
+++ Page-Objects/Pages/Payment_Page.js
@@ -1,17 +1,11 @@
 class Payment_Page {
-  get editShippingAddressLink() {
-    return $("#addressPull");
+    get editShippingAddressLink() {return $("#addressPull");}
+  get editShippingAddressLink() {return $("#addressPull");
   }
-  get creditCardOption() {
-    return $(
-      "#adyen-encrypted-form > div > div.chckt-pm.chckt-pm-card.js-chckt-pm.js-chckt-pm__pm-holder > div.chckt-pm__header.js-chckt-pm__header"
-    );
-  }
-  get payPalOption() {
-    return $("[data-pm='paypal'] .js-chckt-pm__header");
-  }
-  get creditCardNumberField() {
-    return $("#encryptedCardNumber");
+  get creditCardOption() {return $("#adyen-encrypted-form > div > div.chckt-pm.chckt-pm-card.js-chckt-pm.js-chckt-pm__pm-holder > div.chckt-pm__header.js-chckt-pm__header"
+    ); }
+  
+  get creditCardNumberField() {return $("#encryptedCardNumber");
   }
   get creditCardExpiryDate() {
     return $("#encryptedExpiryDate");
@@ -19,18 +13,18 @@ class Payment_Page {
   get creditCardCVV() {
     return $("div>[id =encryptedSecurityCode]");
   }
-  get cardHolderName() {
-    return $(
-      "#card-div > div > div > div.adyen-checkout__loading-input__form._1jpVsksYS5faJOp2y0Tpl4 > div.adyen-checkout__field.adyen-checkout__card__holderName > label > span.adyen-checkout__input-wrapper > input"
+  get cardHolderName() { return $("#card-div > div > div > div.adyen-checkout__loading-input__form._1jpVsksYS5faJOp2y0Tpl4 > div.adyen-checkout__field.adyen-checkout__card__holderName > label > span.adyen-checkout__input-wrapper > input"
     );
   }
-  get placeOrderButton() {
-    return $(
-      "body > div.js-device-info > main > div > div.container.checkout-container.checkout-roving > div.col-xs-12.col-sm-5.col-md-7 > div.adyen-pay-button__wrapper"
+  get placeOrderButton() {return $("body > div.js-device-info > main > div > div.container.checkout-container.checkout-roving > div.col-xs-12.col-sm-5.col-md-7 > div.adyen-pay-button__wrapper"
     );
   }
-  get payPalButton() {
-    return $("#paypal-container");
+
+//get payPalOption() {return $("[data-pm='paypal'] .js-chckt-pm__header]");
+get payPalOption(){return $('//*[@id="adyen-encrypted-form"]//div[@class="chckt-pm chckt-pm-paypal js-chckt-pm "]//div[@class="chckt-pm__header js-chckt-pm__header"]');}
+  
+
+  get payPalButton() {return $("#paypal-container");
   }
 
   get paypalEmail() {
@@ -46,8 +40,7 @@ class Payment_Page {
   }
 
   get acceptPaypalCookiesLink() {
-    return $("#acceptAllButton");
-  }
+    return $("#acceptAllButton"); }
 
   get paypalSubmitButton() {
     return $("#payment-submit-btn");
@@ -76,6 +69,7 @@ class Payment_Page {
 
   placeOrderUsingPaypal() {
     this.editShippingAddressLink.waitForExist();
+    browser.pause(7000);
     this.payPalOption.waitForExist();
     this.payPalOption.scrollIntoView();
     this.payPalOption.click();
@@ -89,12 +83,15 @@ class Payment_Page {
     this.paypalEmail.setValue("sb-rnibz2253238@personal.example.com");
     this.paypalPassword.setValue("9Nja2.Ty");
     this.paypalLoginButton.click();
-    this.acceptPaypalCookiesLink().waitForExist();
-    this.acceptPaypalCookiesLink().click();
+    //this.acceptPaypalCookiesLink().waitForExist();
+   // this.acceptPaypalCookiesLink().click();
+    browser.pause(10000);
     this.paypalSubmitButton.waitForExist();
     this.paypalSubmitButton.scrollIntoView();
     this.paypalSubmitButton.click();
     browser.pause(5000);
+    browser.switchWindow('default');
+    browser.pause(10000);
   }
 }
 export default new Payment_Page();
